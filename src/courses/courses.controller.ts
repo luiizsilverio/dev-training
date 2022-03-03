225c19dd7e4b89848c0236ab62695b40e04d2d1c
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-03 20:50:07 -0300

    First Commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+
+  @Get( 'list' )
+  findAll() {
+    return 'Listagem de cursos'
+  }
+
+}
